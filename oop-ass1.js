9d5aaccd9092b0beb25aff123ecc7697a98cda6e
Ahmad Adelaja <ahmadadelaja@gmail.com>
2016-05-18 02:57:31 +0100

    adds the listNotes function

--- oop-ass1.js
+++ oop-ass1.js
@@ -6,5 +6,11 @@ function NotesApplication(author,notes){
 		this.content=content;
         return (this.notes.push(this.content));
 		};
-		
+	this.listNotes = function(){
+		for (var i=0; i<this.notes.length; i++){
+					pos=this.notes.indexOf(notes[i]);
+					console.log("[Notes ID]:" + "["+ pos +"]" +"\n"+ notes[i]+ "\n"+ "By author: "+ this.author);
+					//console.log(notes[i] + pos);
+			}
+	};
 }
