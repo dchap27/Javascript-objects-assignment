471feacb62c964dc98e9d4f2a2b9e987c37e4a6e
Ahmad Adelaja <ahmadadelaja@gmail.com>
2016-05-18 02:34:59 +0100

    creates the first functionality (create)

--- oop-ass1.js
+++ oop-ass1.js
@@ -1,25 +1,10 @@
 function NotesApplication(author,notes){
 	this.author = author;
-	this.notes = [];
+	this.notes = notes;
 
-	this.create = function(note_content){
-            this.notes.push(note_content);
+	this.create = function(content){
+		this.content=content;
+        return (this.notes.push(this.content));
 		};
 		
-		listNotes = function(note_id,Note_content,author){
-			var note_id = function position(){
-				for (var i=0; i<notes.length; i++){
-					pos=notes.indexOf(notes[i]);
-			}
-			console.log("Note ID: "+note_id);
-			console.log(Note_content);
-			console.log("By Author "+ author);
-		};
-		getNote_id = function(note_id){
-			return content[note_id];
-		};
-	};
-};
 }
-
-Apps = new NotesApplication('ahmad',"how are you doing");
