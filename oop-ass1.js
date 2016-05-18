f9e851e186207a12406e409a618e2042cf34b67e
Ahmad Adelaja <ahmadadelaja@gmail.com>
2016-05-18 04:00:32 +0100

    adds the deletes function

--- oop-ass1.js
+++ oop-ass1.js
@@ -4,17 +4,22 @@ function NotesApplication(author,notes){
 
 	this.create = function(content){
 		this.content=content;
-        return (this.notes.push(this.content));
+        return (this.notes.push(this.content));// adds new content to the list of notes
 		};
 	this.listNotes = function(){
 		for (var i=0; i<this.notes.length; i++){
-					note_id=this.notes.indexOf(notes[i]);
+					note_id=this.notes.indexOf(notes[i]); // gets the index number for each notes
 					console.log("[Notes ID]:" + "["+ note_id +"]" +"\n"+ notes[i]+ "\n"+ "By author: "+ this.author);
 			}
 	};
 	this.getNote = function(note_id){
 		this.note_id = note_id;
-		var contents = this.notes[this.note_id];
-		return contents;
+		return this.notes[this.note_id];
+	};
+
+	this.delete = function(note_id){
+		this.note_id = note_id;
+		this.notes.splice(this.notes.indexOf(notes[note_id]),note_id); // removes the notes with index "note_id"
+		return (this.notes);// returns the new list of notes
 	};
 }
